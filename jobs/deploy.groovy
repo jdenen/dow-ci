05deb55df8ba579593d753ee9b36014a7a6fb6d1
Johnson Denen <johnson.denen@gmail.com>
2016-03-21 23:28:15 -0400

    Add job to deploy OSX builds to s3

---
+++ jobs/deploy.groovy
@@ -0,0 +1,36 @@
+def rubies = ["2.1.6", "2.2.2"]
+
+rubies.each { ruby ->
+  job("ruby-${ruby}-osx-deploy-s3") {
+    description("Upload OSX package to S3")
+
+    triggers {
+      upstream("ruby-${ruby}-osx-smoke-test", "SUCCESS")
+    }
+
+    steps {
+      copyArtifacts("ruby-${ruby}-osx-smoke-test") {
+	includePattern("*.gz")
+	buildSelector {
+	  latestSuccessful(true)
+	}
+      }
+    }
+
+    publishers {
+      slackNotifications {
+	projectChannel("#general")
+	teamDomain("johnson-ci")
+	notifyFailure()
+	notifyRepeatedFailure()
+	notifyBackToNormal()
+	notifySuccess()
+      }
+      s3("dow-release") {
+	entry("*.gz", "dow-release/ruby-${ruby}", "us-east-1") {
+	  noUploadOnFailure()
+	}
+      }
+    }
+  }
+}
